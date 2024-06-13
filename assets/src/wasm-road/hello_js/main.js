6c9d8a9cdcd60360117f5e27c504442729118ae7
Jakob Meier <mail@jakobmeier.ch>
2024-06-13 16:56:18 +0200

    hello_js code, wasm memory access from js
    Extend example to decode a string from wasm memory.

--- assets/src/wasm-road/hello_js/main.js
+++ assets/src/wasm-road/hello_js/main.js
@@ -1,18 +1,37 @@
 async function fetchAndRunWasm() {
-    const wasmModule = await WebAssembly.instantiateStreaming(fetch('hello_js.wasm'), {
-        env: {
-            print_char: (c) => {
-                const char = String.fromCharCode(c);
-                console.log(char);
-            }
+    // Function will be overwritten once we have a memory instance to access.
+    let readWasmString = (_memory) => "";
+
+    // functions to inject into Wasm
+    const imports = {
+        print_char: (c) => {
+            const char = String.fromCharCode(c);
+            console.log(char);
+        },
+        print_string: (start, len) => {
+            const msg = readWasmString(start, len);
+            console.log(msg);
         }
+    };
+
+    // efficiently download and load Wasm module instance
+    const wasm = await WebAssembly.instantiateStreaming(fetch('hello_js.wasm'), {
+        env: imports,
     });
 
-    // Get the exports from the WASM module
-    const { hello_world } = wasmModule.instance.exports;
+    // Get the exports from the WASM module, functions and memory
+    const { hello_world, hello_world_2, memory } = wasm.instance.exports;
 
-    // Call the exported function
+    // overwrite `readWasmString` to access the correct Wasm memory buffer
+    readWasmString = (start, len) => {
+        // Read data directly from Wasm memory
+        const buf = new Uint8Array(memory.buffer, start, len);
+        return new TextDecoder('utf8').decode(buf);
+    }
+
+    // Call the exported functions
     hello_world();
+    hello_world_2();
 }
 
 fetchAndRunWasm();
--- assets/src/wasm-road/hello_js/src/lib.rs
+++ assets/src/wasm-road/hello_js/src/lib.rs
@@ -1,6 +1,9 @@
 extern "C" {
     #[link_name = "print_char"]
     fn js_print_wrapper(c: u32);
+
+    #[link_name = "print_string"]
+    fn js_print_wrapper2(start: *const u8, len: u32);
 }
 
 #[export_name = "hello_world"]
@@ -9,5 +12,13 @@ pub fn hello_world() {
         unsafe {
             js_print_wrapper(c as u32);
         }
+    }
+}
+
+#[export_name = "hello_world_2"]
+pub fn hello_world_2() {
+    static MSG: &str = "hello world!";
+    unsafe {
+        js_print_wrapper2(MSG.as_ptr(), MSG.len() as u32);
     }
 }
